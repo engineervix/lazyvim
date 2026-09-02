62c92ad327648f1699eb3867abe04cfca7f6a3d6
Victor Miti <victor.miti@torchbox.com>
2026-09-02 11:52:06 +0100

    feat: add ThePrimeagen/vim-be-good to practice basic movements
    Vim be good is a plugin designed to make you better
    at vim by creating a game to practice basic movements in.
    
    https://github.com/ThePrimeagen/vim-be-good

---
+++ lua/plugins/vim-be-good.lua
@@ -0,0 +1,6 @@
+return {
+  "ThePrimeagen/vim-be-good",
+  -- Dedicated drill session, not something to keep loaded in real edits;
+  -- lazy-load on the command it exposes.
+  cmd = "VimBeGood",
+}
