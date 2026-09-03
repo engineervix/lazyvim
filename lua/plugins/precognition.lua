a54d0d25f8f6dbc41f9d153b06265001dc8dd411
Victor Miti <victor.miti@torchbox.com>
2026-09-03 17:06:59 +0100

    fix(precognition): move toggle off <leader>up, clashed with Mini Pairs

--- lua/plugins/precognition.lua
+++ lua/plugins/precognition.lua
@@ -1,13 +1,13 @@
 return {
   "tris203/precognition.nvim",
   opts = {
-    -- Off by default; toggle on with <leader>up while drilling motions,
+    -- Off by default; toggle on with <leader>uP while drilling motions,
     -- rather than showing hint virtual text on every line all the time.
     startVisible = false,
   },
   keys = {
     {
-      "<leader>up",
+      "<leader>uP",
       function()
         require("precognition").toggle()
       end,
