cd6987aead583ee87ae2d35e75e171e5aea3263a
Victor Miti <victor.miti@torchbox.com>
2026-09-07 15:52:20 +0100

    feat(python): switch python lsp to basedpyright

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- lang.python extra (enabled in lazyvim.json) reads this at require-time,
+-- so it must be set here (pre-lazy.nvim), not in lua/plugins/*.lua (too late)
+vim.g.lazyvim_python_lsp = "basedpyright"
